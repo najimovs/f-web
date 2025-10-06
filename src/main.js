ca7e3b6e59f0ea692e46e06b5a5e1a61389a77f8
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2025-10-06 15:34:32 +0500

    Add a basic map interface

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 import "@app/ui.css"
+import "mapbox-gl/dist/mapbox-gl.css"
+import mapboxgl from "mapbox-gl"
+
+const MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoibmFqaW1vdiIsImEiOiJjbTBnbWJ3ZGwwMXdqMnFyMXlxY3FsaTJ6In0.TWo-dOdTkiREW-ugZQevpw"
+
+const map = new mapboxgl.Map( {
+	accessToken: MAPBOX_ACCESS_TOKEN,
+	container: "map",
+	center: [ 69.24842475092007, 41.31645300793667 ],
+	zoom: 9,
+	hash: true,
+} )
