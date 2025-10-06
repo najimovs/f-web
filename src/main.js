bfe0543b271f965f919fa9b007dcff4a979eaa03
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2025-10-06 16:14:55 +0500

    Get current (geo) position

--- src/main.js
+++ src/main.js
@@ -12,34 +12,53 @@ const map = new mapboxgl.Map( {
 	hash: true,
 } )
 
-const myPoint = { // GeoJSON
-	type: "Feature",
-	geometry: {
-		type: "Point",
-		coordinates: [ 69.24802011344485, 41.31697426358227 ],
-	},
-}
+
 
 map.on( "load", () => {
 
-	map.addSource( "myPoint", { type: "geojson", data: myPoint } )
+	const button = document.querySelector( "button" )
 
-	map.addLayer( {
-		id: "myPoint",
-		source: "myPoint",
-		type: "circle",
-		paint: {
-			"circle-radius": 10,
-			"circle-color": "#ff00ff",
-			"circle-stroke-width": 4,
-			"circle-stroke-color": "#ffffff",
+	button.onclick = () => {
+
+		const onSucess = position => {
+
+			const { longitude, latitude } = position.coords
+
+			const myPoint = { // GeoJSON
+				type: "Feature",
+				geometry: {
+					type: "Point",
+					coordinates: [ longitude, latitude ],
+				},
+			}
+
+			map.addSource( "myPoint", { type: "geojson", data: myPoint } )
+
+			map.addLayer( {
+				id: "myPoint",
+				source: "myPoint",
+				type: "circle",
+				paint: {
+					"circle-radius": 10,
+					"circle-color": "#ff00ff",
+					"circle-stroke-width": 4,
+					"circle-stroke-color": "#ffffff",
+				}
+			} )
+
+			map.flyTo( {
+				center: [ longitude, latitude ],
+				essential: true,
+			} )
+
+			console.log( "Successfull" )
 		}
-	} )
+
+		const onError = error => {
+
+			console.log( error )
+		}
+
+		navigator.geolocation.getCurrentPosition( onSucess, onError )
+	}
 } )
-
-
-
-
-
-
-
